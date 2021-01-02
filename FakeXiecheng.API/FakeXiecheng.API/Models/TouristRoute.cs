4d8576164b2e162ae134f6115b74e2d047bebb42
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-01-02 21:01:10 +1100

    Change TouristRoute property

--- FakeXiecheng.API/FakeXiecheng.API/Models/TouristRoute.cs
+++ FakeXiecheng.API/FakeXiecheng.API/Models/TouristRoute.cs
@@ -10,10 +10,14 @@ namespace FakeXiecheng.API.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public decimal OriginalPrice { get; set; }
-        public double? DiscountPresent { get; set; }
+        /*
+            可空的，nullable，所以加个问号，问号在C#中表示可空变量，
+            转换为数据库，则代表一个列可以没有初始数据。
+         */
+        public double? DiscountPercent { get; set; }
         public DateTime CreateTime { get; set; }
-        public DateTime UpdateTime { get; set; }
-        public DateTime DepartureTime { get; set; }
+        public DateTime? UpdateTime { get; set; }
+        public DateTime? DepartureTime { get; set; }
         public string Features { get; set; }
         public string Fees { get; set; }
         public string Notes { get; set; }
