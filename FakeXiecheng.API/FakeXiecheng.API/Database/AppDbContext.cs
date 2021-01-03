aa381cdb8d61cbe2acc3b89ec7dc411f57ea21a4
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-01-03 20:24:24 +1100

    Add TouristRoute Seed Data

--- FakeXiecheng.API/FakeXiecheng.API/Database/AppDbContext.cs
+++ FakeXiecheng.API/FakeXiecheng.API/Database/AppDbContext.cs
@@ -3,6 +3,10 @@ using System.Linq;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using FakeXiecheng.API.Models;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+
 
 namespace FakeXiecheng.API.Database
 {
@@ -15,8 +19,28 @@ namespace FakeXiecheng.API.Database
         // 指明哪些数据模型需要映射到数据库中，使用DbSet进行模型的映射
         public DbSet<TouristRoute> TouristRoutes { get; set; }
         public DbSet<TouristRoutePicture> TouristRoutePictures { get; set; }
-
         // 将AppDbContext对象注入到IOC容器中，在startup里面
 
+        // 添加种子数据
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<TouristRoute>().HasData(new TouristRoute()
+            //{
+            //    Id = Guid.NewGuid(),
+            //    Title = "zheshititle",
+            //    Description = "shuoming",
+            //    OriginalPrice = 0,
+            //    CreateTime = DateTime.UtcNow
+            //});
+            var touristRouteJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutesMockData.json");
+            IList<TouristRoute> touristRoutes = JsonConvert.DeserializeObject<IList<TouristRoute>>(touristRouteJsonData);
+            modelBuilder.Entity<TouristRoute>().HasData(touristRoutes);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        // 添加种子数据并更新的命令
+        // dotnet ef migrations add DataSeeding
+        // dotnet ef database update
     }
 }
