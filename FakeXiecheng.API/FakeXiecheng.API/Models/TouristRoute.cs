9f0d688b11e7f368af134aad93a1f4d8548eeb65
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-01-03 18:19:12 +1100

    Add database migration

--- FakeXiecheng.API/FakeXiecheng.API/Models/TouristRoute.cs
+++ FakeXiecheng.API/FakeXiecheng.API/Models/TouristRoute.cs
@@ -1,27 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+// 对数据库进行限定
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FakeXiecheng.API.Models
 {
     public class TouristRoute
     {
         // 基础属性
+        [Key]
         public Guid Id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+        [Required]
+        [MaxLength(1500)]
         public string Description { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal OriginalPrice { get; set; }
         /*
             可空的，nullable，所以加个问号，问号在C#中表示可空变量，
             转换为数据库，则代表一个列可以没有初始数据。
          */
+        [Range(0.0, 1.0)]
         public double? DiscountPercent { get; set; }
         public DateTime CreateTime { get; set; }
         public DateTime? UpdateTime { get; set; }
         public DateTime? DepartureTime { get; set; }
+        [MaxLength]
         public string Features { get; set; }
+        [MaxLength]
         public string Fees { get; set; }
+        [MaxLength]
         public string Notes { get; set; }
         // 外键联系
-        public ICollection<TouristRoutePicture> TouristRoutePictures { get; set; }
+        public ICollection<TouristRoutePicture> TouristRoutePictures { get; set; } = new List<TouristRoutePicture>();
     }
 }
