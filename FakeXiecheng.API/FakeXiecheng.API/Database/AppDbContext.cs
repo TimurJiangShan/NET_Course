bde9093ed5ec19ba86c4f0c60405067f2669566f
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-01-03 15:56:25 +1100

    Config entityframework core and dbconnection

---
+++ FakeXiecheng.API/FakeXiecheng.API/Database/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using FakeXiecheng.API.Models;
+
+namespace FakeXiecheng.API.Database
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        // 指明哪些数据模型需要映射到数据库中，使用DbSet进行模型的映射
+        public DbSet<TouristRoute> touristRoutes { get; set; }
+        public DbSet<TouristRoutePicture> touristRoutePictures { get; set; }
+
+        // 将AppDbContext对象注入到IOC容器中，在startup里面
+
+    }
+}
--- FakeXiecheng.API/FakeXiecheng.API/Startup.cs
+++ FakeXiecheng.API/FakeXiecheng.API/Startup.cs
@@ -2,17 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FakeXiecheng.API.Database;
 using FakeXiecheng.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace FakeXiecheng.API
 {
     public class Startup
     {
+        // 配置依赖注入服务，创建一个私有变量存储配置信息。
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -20,6 +29,11 @@ namespace FakeXiecheng.API
             services.AddControllers();
             // 注册数据仓库的服务依赖
             services.AddTransient<ITouristRouteRepository, MockTouristRouteRepository>();
+            // 向IOC容器中注入Db对象
+            services.AddDbContext<AppDbContext>(option => {
+                //option.UseSqlServer("server=localhost; Database=FakeXiechengDb; User Id=sa; Password=PaSSword12!;");
+                option.UseSqlServer(Configuration["DbContext:ConnectionString"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
