05d36141dc377e503a395c8e937b051adcb533ca
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-01-05 22:21:55 +1100

    Add RESTFUL Notes

--- FakeXiecheng.API/FakeXiecheng.API/Controllers/TouristRoutesController.cs
+++ FakeXiecheng.API/FakeXiecheng.API/Controllers/TouristRoutesController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 // Get请求没有body。可以使用请求头部的header,添加媒体类型, 设置 accept application/json
+// Get请求没有body。可以使用请求头部的header,添加媒体类型, 设置 accept application/json
 
 namespace FakeXiecheng.API.Controllers
 {
